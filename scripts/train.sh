e67d4360c938b3acb804097bbbf20c3637c35b49
EngineerHumo <jiaqi.guo@robotrak.com>
2025-10-16 19:07:34 +0100

    change loss

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 python -u train.py \
   --train_items data/surgical_plan/items/train_items.json \
   --val_items data/surgical_plan/items/val_items.json \
-  --epochs 400 --batch_size 4 --lr 1e-3 \
+  --epochs 400 --batch_size 3 --lr 1e-3 \
   --img_size 1024 1024 --num_queries 6 \
   --save_dir runs/exp0 \
   --visdom
